8f9d6d5cb8d7d7a19f67ff63cd7b9848cf3678ac
Luis <luis@staudt-fridingen.de>
2023-08-18 20:01:46 +0200

    added bool to force aspect ratio to 16/9

--- Assets/Scripts/ShaderScript.cs
+++ Assets/Scripts/ShaderScript.cs
@@ -1,40 +1,51 @@
+using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ShaderScript : MonoBehaviour {
 	public ComputeShader computeShader;
 	public RenderTexture renderTexture;
-	public int textureSize = 256;
+	public int textureSizeX = 256;
+	public int textureSizeY = 256;
 	public int threadDivisor = 8;
 	public int sizeX = 128;
 	public int sizeY = 128;
+	public bool forceAspectRatio;
 	#region Internal
-	private int oldTextureSize;
-	private int oldThreadDivisor;
+	private int oldTextureSizeX;
+	private int oldTextureSizeY;
+	private bool oldForceAspectRatio;
 	private int kernelId;
 	#endregion
 
-	private void OnEnable() {
+	private void OnEnable() 
+	{
 		kernelId = computeShader.FindKernel("CSMain");
 	}
 
 	private void OnRenderImage(RenderTexture source, RenderTexture destination) {
-		if (textureSize != oldTextureSize || threadDivisor != oldThreadDivisor) {
-			renderTexture = new RenderTexture(textureSize, textureSize, 24) {
+		if (textureSizeX != oldTextureSizeX || textureSizeY != oldTextureSizeY || forceAspectRatio != oldForceAspectRatio) {
+			if (forceAspectRatio) {
+				textureSizeY = (textureSizeX / 16) * 9;
+			}
+			renderTexture = new RenderTexture(Math.Max(1, textureSizeX), Math.Max(1, textureSizeY), 24) {
 				enableRandomWrite = true
 			};
+			renderTexture.Create();
 		}
-		
+		if (0 > renderTexture.width || 0 > renderTexture.height) {
+			return;
+		}
 		computeShader.SetTexture(kernelId, "Result", renderTexture);
 		computeShader.SetFloat("Time", Time.time);
 		computeShader.SetFloat("Width", renderTexture.width);
 		computeShader.SetFloat("Height", renderTexture.height);
 		computeShader.SetFloats("Size", sizeX, sizeY);
-
-		computeShader.Dispatch(kernelId, textureSize / threadDivisor, textureSize / threadDivisor, 1);
+        
+		computeShader.Dispatch(kernelId, Math.Max(1, textureSizeX) / Math.Max(1, threadDivisor), Math.Max(1, textureSizeY) / Math.Max(1, threadDivisor), 1);
 		Graphics.Blit(renderTexture, destination);
 		
-		oldTextureSize = textureSize;
-		oldThreadDivisor = threadDivisor;
+		oldTextureSizeX = textureSizeX;
+		oldTextureSizeY = textureSizeY;
+		oldForceAspectRatio = forceAspectRatio;
 	}
 }
