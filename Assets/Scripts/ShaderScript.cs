7a6058d33e6744322fbe11f101c6ff81e2aa4e9d
Luis <luis@staudt-fridingen.de>
2023-08-21 16:59:49 +0200

    cleaned up editor variables

--- Assets/Scripts/ShaderScript.cs
+++ Assets/Scripts/ShaderScript.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 public class ShaderScript : MonoBehaviour {
 	public ComputeShader computeShader;
 	public RenderTexture renderTexture;
-	public int textureSizeX = 1920;
-	public int textureSizeY = 1080;
+	[Min(1)] public int textureSizeX = 1920;
+	[Min(1)] public int textureSizeY = 1080;
 	public bool forceAspectRatio = true;
-	public int threadDivisor = 8;
-	public float sizeX = 1024.0f;
-	public float sizeY = 1024.0f;
-	[Range(0.0f, 360.0f)] 
-	public float degreeStart = 0.0f;
-	[Range(0.0f, 360.0f)] 
-	public float degreeEnd = 360.0f;
-	
+	[Min(1)] public int threadDivisor = 8;
+	[Min(1.0f)] public float sizeX = 1024.0f;
+	[Min(1.0f)] public float sizeY = 1024.0f;
+	[Range(0.0f, 360.0f)] public float degreeStart = 0.0f;
+	[Range(0.0f, 360.0f)] public float degreeEnd = 360.0f;
+
 	#region Internal
 
 	private int oldTextureSizeX;
@@ -33,7 +31,7 @@ public class ShaderScript : MonoBehaviour {
 			if (forceAspectRatio) {
 				textureSizeY = (textureSizeX / 16) * 9;
 			}
-			renderTexture = new RenderTexture(Math.Max(1, textureSizeX), Math.Max(1, textureSizeY), 24) {
+			renderTexture = new RenderTexture(textureSizeX, textureSizeY, 24) {
 				enableRandomWrite = true
 			};
 			renderTexture.Create();
